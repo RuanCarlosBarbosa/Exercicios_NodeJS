0d34ee9c1003ad62d68b0dbdaf3e9069478af545
Ruan <barbosaruan13@gmail.com>
2024-01-18 11:51:46 -0300

    Criando https

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ writeFile("arquivo.txt", "Texto alterado por writeFile", (erro) => {
         console.log("Escreveu com sucesso!");
     }
 });
+
+// utilizando HTTP
+const {createServer} = require('http');
+
+let server = createServer((request, response) => {
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write('\
+    <h1> Hello World </h1>\
+    <p>primeira pagina com Node.JS</p>\
+    ');
+    response.end();
+}); 
+
+server.listen(8000);
+
+console.log("Ouvindo a porta 8000");
